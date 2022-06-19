9a30ab8fc88c603dc67751a8e874c75a6197e8d5
raddad772 <raddad772@raddad.com>
2022-06-18 22:40:47 -0400

    commented assembly program a bunch

--- w65c816/asm_charcodes.js
+++ w65c816/asm_charcodes.js
@@ -1,12 +1,15 @@
 const INIT_ASM = `
-; HI!
+; This section holds configuration data for the assembler
 .config
 # 16 * 65536
 ROM_SIZE $100000
 
+; This section holds vectors for NMI, RESET, etc. if they are set
 .vectors
 RESET EMU_START
 
+; We define a few labels to use as convenient variable names later on.
+; We put them in the zero-page to simplify addressing them.
 .STRPTR:$20
 .FONTPTR:$23
 .SCRPTR:$26
@@ -14,24 +17,29 @@ RESET EMU_START
 .INNERLOOP:$44
 .OUTERLOOP:$46
 
+; Here's where we jump on reset, in emulation mode
 .EMU_START:$2020
-; Let the compiler know it's emulated mode here
+; Let the compiler know it's emulated mode here.
+; A bit of note about program structure, we're testing some basic instructions and making it easy to debug
+;  them, so we'll do a few pointless or silly things.
 :E1
-CLC
-XCE
+CLC   ; Clear C
+XCE   ; eXchange C and E, which will put us in native mode
 
-:NATIVE M1 X1
-JMP >NATIVE_START
+:NATIVE M1 X1     ; let assembler know we're now in native mode with M=1 X=1. It doesn't follow XCE correctly yet
+JMP >NATIVE_START ; This will be a long-jump with 24-bit destination in high bank, to test instructions
 
+; Here is where we store our string
 .HELLOWORLD:$02F000
-;DCB $48, $65, $6c, $6c, $6f, $2c, $20, $77, $6f, $72, $6c, $64, $21, $00
-;ASC "This is so exciting!"
 ASC "Hello, world!"
+; The ASC directive stores an ASCII string as a null-terminated string in memory at the current address.
 
+; This function loads the correct pointers for the "Hello, world!" string and calls the print function
 .PRINT_HELLOWORLD:$040900
-SEP #$30
+SEP #$30     ; Set 8-bit M & X modes
 :E0 M1 X1
-; $0020, $0021, $0022 to have a long-pointer to a null-terminated string.
+; $0020, $0021, $0022 to have a long-pointer to a null-terminated string. We load in $02F000, the address
+;  of our string.
 LDA #$00
 STA <$20
 LDA #$F0
@@ -40,69 +48,70 @@ LDA #$02
 STA <$22
 REP #$30
 ; X and Y to have X and Y coords on screen
-LDX #1
-LDY #5
-JSL >RENDERSTR
-LDA <$AB
+LDX #1          ; Column 1
+LDY #5          ; Row 5
+JSL >RENDERSTR  ; Call the renderstr function
 STP
 
+; This is where we jumped to earlier to test the long-jump instruction
 .NATIVE_START:$040200
-:E0 M1 X1
-REP #$30
-:E0 M0 X0
-LDA #$ABCD  ; Should have done 16-bit load
-STA !$2010   ; Should store 16 bits
-LDA #0
-LDA !$2010
-STA >$010410
-JMP PRINT_HELLOWORLD
+:E0 M1 X1     ; The assembler cannot reliably ascertain the mode after a jump, so we should usually tell it
+REP #$30      ; Right before we go full 16-bit
+LDA #$ABCD    ; Check a 16-bit absolute load works properly, manually
+STA !$2010    ; Check a 16-bit absolute store works properly, manually 
+LDA #0        ; Check a 16-bit immediate load works properly
+LDA !$2010    ; Check that A is now $ABCD again
+STA >$010410  ; Check a 16-bit absolute long store works properly, this will be visible on the screen as 2 pixels
+JMP PRINT_HELLOWORLD ; Now go to our function that prints hello world
 
 
+; RENDERSTR renders an ASCII string to screen.
 
 ; RENDERSTR expects...
 ; $010000-$01FFFF to be a 256x256 monochrome monitor with 1-byte pixels
 ; $0020, $0021, $0022 to have a long-pointer to a null-terminated string.
 ; $0023, $0024, $0025, $0026, $0027, $0028 to be free for usage.
-; This will be mutated until the NULL is reached 
-; X to have x text coord to start
-; Y to have y text coord to start
+; This adrress will be modified until the NULL is reached, so it won't be preserved. 
+; X register has x text coord to start
+; Y register has y text coord to start
 .RENDERSTR:$030010
-; Save flags we're going to restore before returning
+; Save registers we're going to restore before returning
 PHA
-PHB
-PHP
-PHD
+PHB    ; Note this isn't push B, but push DBR
+PHP    ; Easy way to restore previous processor status
+PHD    ; Push D
 
-REP #$30
-:M0 X0
-; Multiply X by 8 and Y by 16! Bound them onto screen!
-TXA
-ASL A
-ASL A
-ASL A
-AND #$FF
-TAX
+REP #$30 ; Set 16-bit index and accumulator
+; Multiply X by 8 and Y by 16! Then AND by 0xFF to make sure the coordinates are on screen!
+TXA       ; X->A
+ASL A     ; A << 1 (*2)
+ASL A     ; A << 1 (*4)
+ASL A     ; A << 1 (*8)
+AND #$FF  ; AND 0xFF
+TAX       ; A->X
 
-TYA
-ASL A
-ASL A
-ASL A
-ASL A
-AND #$FF
-TAY
+TYA       ; Y->A
+ASL A     ; A << 1 (*2)
+ASL A     ; A << 1 (*4)
+ASL A     ; A << 1 (*8)
+ASL A     ; A << 1 (*16)
+AND #$FF  ; AND 0xFF
+TAY       ; A->Y
 
 ; Start algorithm.
-SEP #$20 ; 8-bit memory read & write, high pointer of 2 pointers
-
-; 23-$25 is the font character address
+SEP #$20 ; 8-bit memory read & write
 LDA #$01
 PHA ; Push A to stack...
 PLB ; So that we can update DBR with it
+    ; We want DBR to point to the region we will draw the screen in. This saves us a bunch of cycles
+    ; in the inner loop.
+    ; No you can't just directly transfer.
 
+; At $23-$25 we will store our current pointer into the font data.
+; This is located in data bank 2, so we set the MSB to 0x02
 LDA #$02
 STA <$25
-;STA <$28
-;TCD
+; Zero out some more values just because
 STZ $26
 STZ $27
 STZ $23
@@ -111,71 +120,72 @@ STZ $24
 ; Our actual loop starts here
 .RENDERSTRLOOPSTART
 ; Now, fetch first character into A
-SEP #$20
-LDA [STRPTR] ; Read from string pointer
-BNE RENDERSTRCONT
-JMP RENDERSTRLOOPDONE
-.RENDERSTRCONT
-SEC
-SBC #$20    ; First char is 32
-REP #$20    ; 16-bit M/A
-; Add 1, multiply by 16, subtract 4. This is because they are stored in reversed order, and the top 3 rows are
-;  always blank.
-:M0
-INC A
-ASL A
-ASL A
-ASL A
-ASL A
-SEC
-SBC #4
-STA <FONTPTR
-; Time to calculate screen address. Y * 256 + X.
-; X & Y hold current pixel address
+SEP #$20                ; 8-bit memory read
+LDA [STRPTR]            ; Read from string pointer
+BNE RENDERSTRCONT       ; Continue execution if the charater we read is not NULL
+JMP RENDERSTRLOOPDONE   ; Jump to loop done, if the character was NULL
+.RENDERSTRCONT          ; Continuing execution here...
+SEC         ; We always SET carry before a subtract, if we don't want an extra 1 removed
+SBC #$20    ; First char of font is 32, so we subtract 32
+REP #$20    ; 16-bit memory accesses
+; Now we must add 1, multiply by 16, subtract 4.
+; First, we add 1, because the fonts are stored in reverse row order. So to start drawing from
+;  the top, we need to get to the character AFTER the one we want.
+; Then we multiply by 16, because each character is 16 bytes long.
+; Then we subtract 4. 1 is to get us to the correct character, and 3 is to skip 3 lines that are always blank. 
+INC A        ; +1
+ASL A        ; << 1 (*2)
+ASL A        ; << 2 (*4)
+ASL A        ; << 3 (*8)
+ASL A        ; << 4 (*16)
+SEC          ; Set Carry
+SBC #4       ; Subtract 4
+STA <FONTPTR ; Store in our FONT POINTER
+; Time to calculate memory address of the screen coordinates. We do this by multiplying Y by 256 and adding X.
+; X & Y hold current pixel address.
 TYA              ; Y->A
-XBA              ; * 256
-CLC
+XBA              ; This exchanges upper and lower bytes of A. Effectively * 256 if upper byte is clear.
 STX <TMPVAR      ; X->TMPVAR
-ADC <TMPVAR      ; Y*256+X
+CLC              ; Clear carry before ADDC
+ADC <TMPVAR      ; A+TMPVAR(X)
 STA <SCRPTR      ; Set our screen pointer to that
 
 ; Now we must render 13 lines of 8 pixels! YAY!
+; The upper 3 lines are never ever used, so why render the empty lines? 
 LDA #13
 STA <OUTERLOOP   ; Outer loop will be 13 long
 .RS_OUTER_START  ; Outer loop starts here
-SEP #$20
-LDA [<FONTPTR]   ; Load this row of pixels
-BEQ CLRLINE      ; If this row of pixels is empty, go to a specialized part that will empty it for us
-STA <TMPVAR      ; Store in our temporary variable
+SEP #$20         ; Make sure to set 8-bit memory access
+LDA [<FONTPTR]   ; Load this row of pixels, stored as 1 bit per pixel
+BEQ CLRLINE      ; If this row of pixels is empty, go to a specialized section that will draw it faster
+STA <TMPVAR      ; Store the pixels in our temporary variable, we don't want to modify the font itself
 
-; Render 8 pixels
+; Render 8 pixels.
+; I unrolled the loop for a few reasons. Not only does it save on branch-related cycles, but it allows me
+;  to keep certain variables in the very small number of registers, which saves a lot more cycles, since I
+;  no longer need those registers to process decrementing the loop counter.
+; First a few preparations  
 REP #$20      ; Set 16-bit memory mode
 PHX           ; Store X for later
 LDX <SCRPTR   ; Load screen pointer to X
+
+; Now, the first iteration of the loop, which will be done 4 times total.
 LDA #0        ; Clear A
-ROR <TMPVAR   ; Rotate out rightmost pixel
-ROL A         ; Rotate into A
-XBA           ; Swap A & B
-ROR <TMPVAR   ; Rorate out next rightmost pixel
-ROL A         ; Rotate into A
-STA !6,x      ; Store at positions 6&7 since we're going right to left now
+ROR <TMPVAR   ; Rotate out rightmost pixel to the Carry flag
+ROL A         ; Rotate it into A
+XBA           ; Swap A & B, leaving the high byte with the pixel and the low byte empty
+ROR <TMPVAR   ; Rorate out next rightmost pixel into carry flag
+ROL A         ; Rotate into A. B + A is now two bytes representing two pixels. B is 0x02 if there's a pixel instead of 0x01 but that doesn't matter
+STA !6,x      ; Store at positions 6&7 since we're going right to left. Use direct indexed by x addressing mode, to save cycles and keep us from doing unncessary arithmetic.
 
-; Then repeat 3 more times for gorups of 2 pixels
+; Then repeat 3 more times for groups of 2 pixels
 LDA #0
 ROR <TMPVAR
 ROL A
 XBA
 ROR <TMPVAR
 ROL A
-STA !4,x
-
-LDA #0
-ROR <TMPVAR
-ROL A
-XBA
-ROR <TMPVAR
-ROL A
-STA !2,x
+STA !4,x     ; Positions 4&5 now
 
 LDA #0
 ROR <TMPVAR
@@ -183,31 +193,41 @@ ROL A
 XBA
 ROR <TMPVAR
 ROL A
-STA !0,x
+STA !2,x     ; 2 & 3
 
-TXA         ; Gotta do a little math on X
-ADC #256    ; Represent the 8 pixels we've drawn, plus the 248 from below
-PLX         ; Restore X from before
+LDA #0
+ROR <TMPVAR
+ROL A
+XBA
+ROR <TMPVAR
+ROL A
+STA !0,x    ; 0 & 1
+
+TXA         ; Gotta do a little math on SCRPTR. Instead of recalculating the memory coordinates, just advance by
+            ; 1 row.
+ADC #256    ; Each row is 256 bytes
+PLX         ; Restore X we saved before
 
 .INNERLOOPSKIP
-; So now we're on the next row. 
-; Increase pixel address by 248 to get to next row
-STA <SCRPTR
+STA <SCRPTR ; Store our newly-calculated screen memory address. We do it here because the CLRLINE subfunction works that way.
 
-; Decrease font address by 1, because remember it's stored in backwards order for some reason 
-DEC <FONTPTR
-DEC <OUTERLOOP
-BNE RS_OUTER_START
 
-REP #$20
-INC <STRPTR
+DEC <FONTPTR       ; Decrease font address by 1, because remember it's stored in backwards order for some reason
+DEC <OUTERLOOP     ; Decrease our outer loop counter
+BNE RS_OUTER_START ; If we're not at zero, go back to loop start
+
+; We've completed a character, time to do a few things...
+REP #$20      ; Set 16-bit memory accesses
+INC <STRPTR   ; Increment the pointer into the NULL-terminated string we're printing
 ; Increase screen X coord
-TXA
-ADC #10
-TAX
-SEP #$20
+TXA           ; X->A
+; Note we skip a CLC here, because our last arithmetic operation, INC <STRPTR, will never set carry, unless we
+;  have a 65,536-character-long string.
+ADC #10       ; Font is 8 pixels wide, but fills up all 8, so we give 1 extra pixel on each side.
+TAX           ; A->X
 JMP RENDERSTRLOOPSTART
 
+; This will write an empty line out a bit faster than the equivalent generic code
 .CLRLINE
 REP #$20
 LDA #0
@@ -220,9 +240,9 @@ STA !0,x
 TXA
 ADC #256
 PLX
-BRA INNERLOOPSKIP
+BRA INNERLOOPSKIP ; BRA a little faster than JMP here
 
-; Loop done! Restore everything and return long
+; We end up here when the print function is finished
 .RENDERSTRLOOPDONE
 REP #$30
 PLD
@@ -235,6 +255,8 @@ RTL
 ; from https://courses.cs.washington.edu/courses/cse457/98a/tech/OpenGL/font.c
 ; our font will be placed in 16-character blocks 
 ; start at 32 for space
+; note: this is stored in the exact opposite way you'd expect, right-to-left and bottom-to-top.
+; you'd generally expect left-to-right and top-to-bottom. made the algorithm to draw a little more fun.
 .FONT:$020000
 DCB $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00 
 ;!, $020010
