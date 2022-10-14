14bc1a5385e4e0a5983fc5aec351e273b2b51be8
David Schneider <dave@praisingod.com>
2022-10-14 16:36:28 -0400

    changing TypedArray to StaticArray and putting unchecked() around everything cut frametime roughly in half so far

--- assemblyscript/assembly/system/nes/mappers/mapper_none.ts
+++ assemblyscript/assembly/system/nes/mappers/mapper_none.ts
@@ -6,10 +6,10 @@ export class NES_mapper_none implements NES_mapper {
     clock: NES_clock
     bus: NES_bus
     a12_watcher: NES_a12_watcher_t
-    CHR_ROM: Uint8Array
-    PRG_ROM: Uint8Array
-    CIRAM: Uint8Array
-    CPU_RAM: Uint8Array
+    CHR_ROM: StaticArray<u8>
+    PRG_ROM: StaticArray<u8>
+    CIRAM: StaticArray<u8>
+    CPU_RAM: StaticArray<u8>
     ppu_mirror: u32
     PRG_ROM_size: u32
     constructor(clock: NES_clock, bus: NES_bus) {
@@ -17,12 +17,12 @@ export class NES_mapper_none implements NES_mapper {
         this.bus = bus;
         this.a12_watcher = new NES_a12_watcher_t(clock);
 
-        this.CHR_ROM = new Uint8Array(0x2000);
-        this.PRG_ROM = new Uint8Array(0x8000);
+        this.CHR_ROM = new StaticArray<u8>(0x2000);
+        this.PRG_ROM = new StaticArray<u8>(0x8000);
         this.PRG_ROM_size = 0
-        this.CIRAM = new Uint8Array(0x2000); // PPU RAM
-        this.CPU_RAM = new Uint8Array(0x800);
-        this.ppu_mirror = 0
+        this.CIRAM = new StaticArray<u8>(0x2000); // PPU RAM
+        this.CPU_RAM = new StaticArray<u8>(0x800);
+        this.ppu_mirror = 0;
     }
 
     mirror_ppu_addr(addr: u32): u32 {
@@ -41,20 +41,20 @@ export class NES_mapper_none implements NES_mapper {
     CPU_read(addr: u32, val: u32, has_effect: u32): u32 {
         addr &= 0xFFFF;
         if (addr < 0x2000)
-            return this.CPU_RAM[addr & 0x7FF];
+            return unchecked(this.CPU_RAM[addr & 0x7FF]);
         if (addr < 0x3FFF)
             return this.bus.PPU_reg_read(addr, val, has_effect);
         if (addr < 0x4020)
             return this.bus.CPU_reg_read(addr, val, has_effect);
         if (addr >= 0x8000)
-            return this.PRG_ROM[(addr - 0x8000) % this.PRG_ROM_size];
+            return unchecked(this.PRG_ROM[(addr - 0x8000) % this.PRG_ROM_size]);
         return val;
     }
 
     CPU_write(addr: u32, val: u32): void {
         addr &= 0xFFFF;
         if (addr < 0x2000) {
-            this.CPU_RAM[addr & 0x7FF] = val;
+            unchecked(this.CPU_RAM[addr & 0x7FF] = <u8>val);
             return;
         }
         if (addr < 0x3FFF) {
@@ -69,21 +69,25 @@ export class NES_mapper_none implements NES_mapper {
 
     PPU_read(addr: u32, val: u32, has_effect: u32): u32 {
         if (addr < 0x2000)
-            return this.CHR_ROM[addr];
-        return this.CIRAM[this.mirror_ppu_addr(addr)];
+            return unchecked(this.CHR_ROM[addr]);
+        return unchecked(this.CIRAM[this.mirror_ppu_addr(addr)]);
     }
 
     PPU_write(addr: u32, val: u32): void {
         if (addr < 0x2000) return;
-        this.CIRAM[this.mirror_ppu_addr(addr)] = val;
+        unchecked(this.CIRAM[this.mirror_ppu_addr(addr)] = <u8>val);
     }
 
     reset(): void { } // naught to do on reset
 
     set_cart(cart: NES_cart): void {
-        this.CHR_ROM.set(cart.CHR_ROM);
-        this.PRG_ROM = new Uint8Array(cart.PRG_ROM.byteLength);
-        this.PRG_ROM.set(cart.PRG_ROM);
+        for (let i = 0; i < 0x2000; i++) {
+            this.CHR_ROM[i] = cart.CHR_ROM[i];
+        }
+        this.PRG_ROM = new StaticArray<u8>(cart.PRG_ROM.byteLength);
+        for (let i = 0, k = cart.PRG_ROM.byteLength; i < k; i++) {
+            this.PRG_ROM[i] = cart.PRG_ROM[i];
+        }
         this.PRG_ROM_size = cart.header.prg_rom_size;
 
         this.ppu_mirror = cart.header.mirroring;
