1d07803db74a8e3ae5e877db7fc94997dee2540e
David Schneider <dave@praisingod.com>
2022-10-16 19:23:46 -0400

    MMC3b assemblyscript is on its way

--- assemblyscript/assembly/system/nes/mappers/interface.ts
+++ assemblyscript/assembly/system/nes/mappers/interface.ts
@@ -19,14 +19,46 @@ export interface systemEmulator {
 import {NES_bus, NES_clock} from "../nes_common";
 import {NES_cart} from "../nes_cart";
 
+export enum NES_a12_watcher_edge {
+    nothing = 0,
+    rise,
+    fall
+}
+
 export class NES_a12_watcher_t {
     clock: NES_clock
+    cycles_down: u32 = 0;
+    last_cycle: u32 = 0;
+    delay: u32 = 3;
+
     constructor(clock: NES_clock) {
         this.clock = clock;
     }
 
-    update(addr: u32): u32 {
-        return 0;
+    update(addr: u32): NES_a12_watcher_edge {
+        let result: NES_a12_watcher_edge = NES_a12_watcher_edge.nothing;
+        let ppufc: u32 = this.clock.ppu_frame_cycle;
+        if (this.cycles_down > 0) {
+            if (this.last_cycle > ppufc)
+                this.cycles_down += (89342 - this.last_cycle) + ppufc;
+            else
+                this.cycles_down += ppufc - this.last_cycle;
+        }
+
+        if ((addr & 0x1000) === 0) {
+            if (this.cycles_down === 0) {
+                this.cycles_down = 1;
+                result = NES_a12_watcher_edge.fall;
+            }
+        }
+        else {
+            if (this.cycles_down > this.delay)
+                result = NES_a12_watcher_edge.rise;
+            this.cycles_down = 0;
+        }
+        this.last_cycle = ppufc;
+
+        return result;
     }
 }
 
