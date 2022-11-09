ed23e409d7e68d6715b14f991f7d22d662e5f2c0
David Schneider <dave@praisingod.com>
2022-11-09 11:11:00 -0500

    assemblyscript now uses actual generated esm bindings again thanks Discord!

--- helpers/as_wrapper.js
+++ helpers/as_wrapper.js
@@ -1,52 +1,40 @@
 "use strict";
 
 const USE_DEBUG = false;
-if (DO_WASM_IMPORTS) {
-    if (USE_DEBUG)
-        importScripts('/assemblyscript/build/debug_stable.js');
-    else
-        importScripts('/assemblyscript/build/release_stable.js');
-}
 
 class gp_wrapper_t {
     constructor() {
-        this.wasm_fetch = null;
         this.wasm = null;
-        this.wasm_raw = null;
         this.setup = false;
         this.input_buffer_ptr = null;
 
         this.global_player = null;
 
         this.out_ptr = 0;
-        console.log('GP WRAPPER LOAD!');
     }
 
     async onload() {
         if (!this.setup) await this.do_setup();
     }
 
+    async import_wasm() {
+        let url;
+        if (USE_DEBUG)
+            url = '/assemblyscript/build/debug.js';
+        else
+            url = '/assemblyscript/build/release.js';
+
+        this.wasm = await import(url);
+    }
+
     async do_setup() {
         if (this.setup) return;
-        await this.fetch_wasm();
-        await this.instantiate();
+
+        await this.import_wasm();
+
         this.global_player = this.wasm.new_global_player();
         this.input_buffer_ptr = this.wasm.gp_get_input_buffer(this.global_player);
         this.setup = true;
-    }
-
-    async instantiate() {
-        this.wasm_raw = await WebAssembly.compileStreaming(this.wasm_fetch)
-        this.wasm = await instantiate(this.wasm_raw, {})
-    }
-
-    async fetch_wasm() {
-        let url = '';
-        if (USE_DEBUG)
-            url = '/assemblyscript/build/debug.wasm'
-        else
-            url = '/assemblyscript/build/release.wasm'
-        this.wasm_fetch = await fetch(url);
     }
 
     /**
