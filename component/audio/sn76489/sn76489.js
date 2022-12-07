b4cf225a19f230dc189c4611e9063d77bb735ed4
David Schneider <dave@praisingod.com>
2022-12-07 16:17:49 -0500

    first pass at abstracted audio output class for emulators

--- component/audio/sn76489/sn76489.js
+++ component/audio/sn76489/sn76489.js
@@ -12,40 +12,52 @@ class SN76489_SW {
     }
 }
 
-class SN76489 {
+class JSM_audiobuffer {
     constructor() {
-        this.sample_rate = 48000;
         this.next_buf_sample = 0;
-        this.cycles_per_frame = 179208
         this.current_buf = new Int16Array();
         this.current_buf_num = 0;
         this.last_finished_buf = -1;
         this.sample_bufs = [];
+        this.clocks_per_sample = 0;
+        this.buf_pos = 0;
+        this.buf_size = 0;
+        this.cycles_per_frame = 0;
+    }
+
+    set_audio_params(samples_per_frame, sample_rate, buf_size, num_bufs) {
+        this.buf_size = buf_size;
+        this.num_bufs = num_bufs;
+        this.cycles_per_frame = samples_per_frame;
 
         this.buf_pos = 0;
 
-        this.master_clock_speed = 10752480;
-        this.last_sample_time = 0;
+        this.sample_bufs = [];
 
-        // he SN76489 has 8 "registers" - 4 x 4 bit volume registers, 3 x 10 bit
-        // tone registers and 1 x 4 bit noise register*.
-        this.vol = [0x0F, 0x0F, 0x0F, 0x07]; // Volume 1-3 and noise. noise is only 3 bits
-        this.freq = [0, 0, 0];   // 10 bits
-        this.noise = {
-            lfsr: 0x8000,
-            shift_rate: 0,
-            mode: 0,
-            counter: 0,
-            countdown: 0
-        };
-        this.sw = [new SN76489_SW(), new SN76489_SW(), new SN76489_SW()]
-        this.polarity = [1, 1, 1, 1];
-
-        this.clocks_per_sample = this.cycles_per_frame / 800;
-        this.io = {
-            reg: 0,   // Current register is 0
-            kind: 0, // 0 = tone, volume = 1
+        for (let i = 0; i < num_bufs; i++) {
+            this.sample_bufs[i] = new Int16Array(buf_size);
         }
+        this.current_buf = this.sample_bufs[0];
+        this.last_finished_buf = -1;
+        this.current_buf_num = 0;
+        this.clocks_per_sample = this.cycles_per_frame / buf_size;
+    }
+
+    sample(sample) {
+        this.next_buf_sample += this.clocks_per_sample;
+
+        this.current_buf[this.buf_pos] = sample;
+        this.buf_pos++;
+        if (this.buf_pos > this.buf_size) {
+            this.advance_buf()
+        }
+    }
+
+    advance_buf() {
+        if (this.last_finished_buf === -1) this.last_finished_buf = this.current_buf_num;
+        this.current_buf_num = (this.current_buf_num + 1) % this.num_bufs;
+        this.current_buf = this.sample_bufs[this.current_buf_num];
+        this.buf_pos = 0;
     }
 
     get_buffer() {
@@ -61,6 +73,31 @@ class SN76489 {
         this.last_finished_buf = (this.last_finished_buf + 1) % this.num_bufs;
         if (this.last_finished_buf === this.current_buf_num) this.last_finished_buf = -1;
         return ob;
+    }
+}
+
+class SN76489 {
+    constructor() {
+        this.output = new JSM_audiobuffer();
+
+        // he SN76489 has 8 "registers" - 4 x 4 bit volume registers, 3 x 10 bit
+        // tone registers and 1 x 4 bit noise register*.
+        this.vol = [0x0F, 0x0F, 0x0F, 0x07]; // Volume 1-3 and noise. noise is only 3 bits
+        this.freq = [0, 0, 0];   // 10 bits
+        this.noise = {
+            lfsr: 0x8000,
+            shift_rate: 0,
+            mode: 0,
+            counter: 0,
+            countdown: 0
+        };
+        this.sw = [new SN76489_SW(), new SN76489_SW(), new SN76489_SW()]
+        this.polarity = [1, 1, 1, 1];
+
+        this.io = {
+            reg: 0,   // Current register is 0
+            kind: 0, // 0 = tone, volume = 1
+        }
     }
 
     cycle_squares() {
@@ -81,7 +118,6 @@ class SN76489 {
     }
 
     cycle_noise() {
-        //if (this.noise.counter === 0) return;
         this.noise.counter--;
         if (this.noise.counter <= 0) {
             let multiplier = 1;
@@ -119,17 +155,15 @@ class SN76489 {
     }
 
     cycle(current_clock) {
-        // We are not mixing samples here, we're changing tone outputs.
         // Tick the noise
         this.cycle_noise();
         this.cycle_squares();
-        if (current_clock >= this.next_buf_sample) {
-            this.next_buf_sample += this.clocks_per_sample;
-            this.sample();
+        if (current_clock >= this.output.next_buf_sample) {
+            this.output.sample(current_clock, this.mix_sample());
         }
     }
 
-    sample() {
+    mix_sample() {
         let sample = 0;
         for (let i = 0; i < 3; i++) {
             //let intensity = 8192 * ((0x0F - this.vol[i]) / 0x0F);
@@ -143,38 +177,7 @@ class SN76489 {
         let intensity = SMSGG_voltable[this.vol[3]];
         sample += ((this.polarity[3] * 2) - 1) * intensity;
 
-        // Mix
-        this.current_buf[this.buf_pos] = sample;
-        this.buf_pos++;
-        if (this.buf_pos > this.buf_size) {
-            this.advance_buf()
-        }
-    }
-
-    advance_buf() {
-        if (this.last_finished_buf === -1) this.last_finished_buf = this.current_buf_num;
-        this.current_buf_num = (this.current_buf_num + 1) % this.num_bufs;
-        this.current_buf = this.sample_bufs[this.current_buf_num];
-        this.buf_pos = 0;
-    }
-
-    set_audio_params(sample_rate, buf_size, num_bufs) {
-        console.log('AUDIO PARAMS', sample_rate, buf_size, num_bufs);
-        this.sample_rate = sample_rate;
-        this.buf_size = buf_size;
-        this.num_bufs = num_bufs;
-
-        this.buf_pos = 0;
-
-        this.sample_bufs = [];
-
-        for (let i = 0; i < num_bufs; i++) {
-            this.sample_bufs[i] = new Int16Array(buf_size);
-        }
-        this.current_buf = this.sample_bufs[0];
-        this.last_finished_buf = -1;
-        this.current_buf_num = 0;
-        this.clocks_per_sample = this.cycles_per_frame / buf_size;
+        return sample;
     }
 
     write_data(val) {
